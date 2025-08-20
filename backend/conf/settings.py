bb04fa8a22676e6abba06b15a4e86c129acf1ffa
eunbyul cho <eunbyulcho@eunbyului-MacBookPro.local>
2025-08-20 23:35:39 +0900

    settings_fix

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api',
-    'apps.contacts'
+    'django_filters',
+    'drf_spectacular',
+    'apps.contacts',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
